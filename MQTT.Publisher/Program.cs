50f1fc33e03c40495b1436a6c2734ae07a62dc6d
umutSahinkaya <umut.sahinkaya@makel.com.tr>
2024-05-20 14:02:45 +0300

    For ile yapıldı şimdi parallel For ile nasıl bir sonuç alacağımıza bakalım

--- MQTT.Publisher/Program.cs
+++ MQTT.Publisher/Program.cs
@@ -1,41 +1,75 @@
-﻿using MQTTnet.Client.Options;
+﻿using System;
+using System.Threading.Tasks;
+using Data;
 using MQTTnet;
 using MQTTnet.Client;
+using MQTTnet.Client.Options;
 
-namespace MQTT.Publisher;
-class Program
+namespace MQTT.Publisher
 {
-    public static async Task Main(string[] args)
+    class Program
     {
-        var factory = new MqttFactory();
-        var mqttClient = factory.CreateMqttClient();
-
-        var options = new MqttClientOptionsBuilder()
-            .WithClientId("PublisherClient")
-            .WithTcpServer("localhost", 1883) // MQTT broker adresi ve portu
-            .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V500)
-            .Build();
-
-        mqttClient.UseConnectedHandler(async e =>
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Publisher connected successfully.");
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            var data = LongReadOut.Message;
+            const int sayac = 10000;
 
-            // Mesaj yayınlama
-            var message = new MqttApplicationMessageBuilder()
-                .WithTopic("shared/topic")
-                .WithPayload("Hello Shared Subscription")
-                .WithExactlyOnceQoS()
-                .WithRetainFlag(false)
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId("PublisherClient")
+                .WithTcpServer("localhost", 1883) // MQTT broker adresi ve portu
+                .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V500)
                 .Build();
 
-            await mqttClient.PublishAsync(message, CancellationToken.None);
-        });
+            await Task.Delay(2000);
+            mqttClient.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("Publisher connected successfully.");
 
-        await mqttClient.ConnectAsync(options, CancellationToken.None);
+                for (int index = 0; index < sayac; index++)
+                {
+                    var message = new MqttApplicationMessageBuilder()
+                         .WithTopic("topic/test") // Topic adı
+                         .WithPayload($"{data} - {index + 1}")
+                         .WithAtLeastOnceQoS()
+                         .WithRetainFlag(false)
+                         .Build();
+                    try
+                    {
+                        await mqttClient.PublishAsync(message, CancellationToken.None);
+                        Console.WriteLine($"Mesaj yayımlandı: {index + 1} kez");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Mesaj yayınlama sırasında bir hata oluştu: {ex.Message}");
+                    }
+                }
+            });
 
-        Console.WriteLine("Press any key to exit.");
-        Console.ReadLine();
+            mqttClient.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Publisher disconnected.");
+                if (e.Exception != null)
+                {
+                    Console.WriteLine($"Bağlantı kesilme nedeni: {e.Exception.Message}");
+                }
+            });
 
-        await mqttClient.DisconnectAsync();
+            try
+            {
+                await mqttClient.ConnectAsync(options, CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Bağlantı sırasında bir hata oluştu: {ex.Message}");
+                return;
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+
+            await mqttClient.DisconnectAsync();
+        }
     }
 }
--- MQTT.Subscriber/Program.cs
+++ MQTT.Subscriber/Program.cs
@@ -2,11 +2,13 @@
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Options;
 
-namespace MqttSubscriber
+namespace MQTT.Subscriber
 {
     class Program
     {
@@ -21,30 +23,85 @@ namespace MqttSubscriber
                 .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V500)
                 .Build();
 
+            // MongoDB'ye bağlan
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("MyDatabase");
+            var collection = database.GetCollection<BsonDocument>("LongReadCollection");
+
             mqttClient.UseConnectedHandler(async e =>
             {
                 Console.WriteLine("Subscriber connected successfully.");
 
                 // Shared Subscription ile abone olma
                 await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder()
-                    .WithTopic("$share/group1/shared/topic")
+                    .WithTopic("$share/group1/topic/test") // Topic adı
                     .Build());
 
-                Console.WriteLine("Subscribed to $share/group1/shared/topic with shared subscription.");
+                Console.WriteLine("Subscribed to $share/group1/topic/test with shared subscription.");
             });
 
             mqttClient.UseDisconnectedHandler(e =>
             {
                 Console.WriteLine("Subscriber disconnected.");
+                if (e.Exception != null)
+                {
+                    Console.WriteLine($"Bağlantı kesilme nedeni: {e.Exception.Message}");
+                }
             });
 
-            mqttClient.UseApplicationMessageReceivedHandler(e =>
+            mqttClient.UseApplicationMessageReceivedHandler(async e =>
             {
-                var message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
-                Console.WriteLine($"Received message: {message} from topic: {e.ApplicationMessage.Topic}");
+                try
+                {
+                    string topic = e.ApplicationMessage.Topic;
+                    string payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+
+                    switch (topic)
+                    {
+                        case "$share/group1/topic/test":
+                            var document = new BsonDocument
+                            {
+                                { "Message", payload }
+                            };
+                            await collection.InsertOneAsync(document);
+                            Console.WriteLine("topic/test topic'ine ait veri MongoDB'ye kaydedildi.");
+                            break;
+                        case "/topic/test":
+                             document = new BsonDocument
+                            {
+                                { "Message", payload }
+                            };
+                            await collection.InsertOneAsync(document);
+                            Console.WriteLine("topic/test topic'ine ait veri MongoDB'ye kaydedildi.");
+                            break;
+                        case "topic/test":
+                             document = new BsonDocument
+                            {
+                                { "Message", payload }
+                            };
+                            await collection.InsertOneAsync(document);
+                            Console.WriteLine("topic/test topic'ine ait veri MongoDB'ye kaydedildi.");
+                            break;
+                        default:
+                            Console.WriteLine("Böyle bir Topic YOK.");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Bir hata ile karşılaşıldı: {ex.Message}");
+                }
             });
 
-            await mqttClient.ConnectAsync(options, CancellationToken.None);
+            try
+            {
+                await mqttClient.ConnectAsync(options, CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Bağlantı sırasında bir hata oluştu: {ex.Message}");
+                return;
+            }
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadLine();
